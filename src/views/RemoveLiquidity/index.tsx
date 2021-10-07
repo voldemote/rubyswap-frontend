84ec74e182b06cd0a763356dc9ef54ce91586689
tolmany <jollydev0821@gmail.com>
2021-10-07 17:40:05 +0800

    fix liquidity

--- src/views/RemoveLiquidity/index.tsx
+++ src/views/RemoveLiquidity/index.tsx
@@ -112,7 +112,7 @@ export default function RemoveLiquidity({
       { name: 'verifyingContract', type: 'address' },
     ]
     const domain = {
-      name: 'Pancake LPs',
+      name: 'Ruby LPs',
       version: '1',
       chainId,
       verifyingContract: pair.liquidityToken.address,
